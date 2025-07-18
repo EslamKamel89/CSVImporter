84129083e1dfc6b09e3c4272a60bc9344dd20ec4
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-18 12:50:47 +0300

    Optimize CsvRowProcessed: implement ShouldBroadcastNow and fix post-login redirect

--- app/Events/CsvRowProcessed.php
+++ app/Events/CsvRowProcessed.php
@@ -7,10 +7,11 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class CsvRowProcessed {
+class CsvRowProcessed implements ShouldBroadcastNow {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     /**
@@ -36,7 +37,8 @@ class CsvRowProcessed {
      */
     public function broadcastOn(): array {
         return [
-            new PrivateChannel('csv-progress'),
+            // new PrivateChannel('csv-progress'),
+            new Channel('csv-progress'),
         ];
     }
 }
