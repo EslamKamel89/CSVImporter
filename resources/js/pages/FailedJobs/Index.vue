edeb965075bde0f6e7a7d5f9ebac9b7300e32d25
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-27 15:29:53 +0300

    feat(ui): add failed jobs table

--- resources/js/pages/FailedJobs/Index.vue
+++ resources/js/pages/FailedJobs/Index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
@@ -26,7 +27,29 @@ defineProps<{
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex flex-col flex-1 h-full gap-4 p-4 overflow-x-auto rounded-xl">
             <h2>Failed jobs</h2>
-            <pre>{{ jobs }}</pre>
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead class="w-[100px]"> Job Name </TableHead>
+                        <TableHead>Queue</TableHead>
+                        <TableHead class="w-[100px]">exception</TableHead>
+                        <TableHead class="text-right"> Failed at </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow v-for="job in jobs" :key="job.id">
+                        <TableCell class="font-medium">
+                            {{ job.job_name }}
+                        </TableCell>
+                        <TableCell>{{ job.queue }}</TableCell>
+                        <TableCell class="line-clamp-4 max-w-[200px] text-ellipsis">{{ job.exception }}</TableCell>
+                        <TableCell class="text-right">
+                            {{ job.failed_at }}
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
         </div>
     </AppLayout>
 </template>
